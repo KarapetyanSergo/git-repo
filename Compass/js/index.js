21fc13669d66eaa698f0dcea3a12bffdc93bea86
KarapetyanSergo <sergo.karapetyan.00@mail.ru>
2018-04-13 03:08:45 -0700

    js code

--- Compass/js/index.js
+++ Compass/js/index.js
@@ -1,8 +1,21 @@
 jQuery(document).ready(function ($) {
+    $(document).mousemove(function (event) {
+        var head;
+        function onSuccess(heading) {
+            head = heading.magneticHeading;
+            console.log(head);
+            $("#arrow").css({"transform": "rotate(" + head + "deg)"})
+        }
 
-$(document).mousemove(function (event) {
-    event = window.event;
-    var kord = event.pageX;
-    $("#arrow").css({"transform": "rotate("+kord+"deg)"})
-});
+        function onError(compassError) {
+            alert('Compass Error: ' + compassError.code);
+        }
+
+        function onDeviceReady() {
+            navigator.compass.getCurrentHeading(onSuccess, onError);
+        }
+
+        document.addEventListener("deviceready", onDeviceReady, false);
+
+    });
 });
